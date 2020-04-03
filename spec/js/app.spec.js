e7a9ca91388ffbce1e5efbd969858a3b9f9a8738
ja-br <letstalk@jamesbrooks.tech>
2020-04-02 23:25:07 -0400

    Basics of character constructor have been made

---
+++ spec/js/app.spec.js
@@ -0,0 +1,21 @@
+describe('EverCraft Character', () => {
+
+    beforeEach(()=>{
+
+        testCharacter = new EverCraftCharacter("Tom", "evil")
+    })
+    describe('Character Core', ()=>{
+        it('should have a name', () => {
+            expect(testCharacter.name).toBe("Tom")
+        });
+        it('should have an alignment', () => {
+            expect(testCharacter.alignment).toBe("evil")
+        });
+        it('should have an armor class of 10', () => {
+            expect(testCharacter.armorClass).toBe(10)
+        });
+        it('should have 5 hit points', () => {
+            expect(testCharacter.hitPoints).toBe(5)
+        });
+    })
+});
---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+class EverCraftCharacter{
+    constructor(name, alignment){
+        this.name = name
+        this.alignment = alignment
+        this.armorClass = 10
+        this.hitPoints = 5
+        
+    }
+
+}
